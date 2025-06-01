c689822f45569ecc0d77a5e980128df78246e0a3
Lucas <lucastavaresbento@gmail.com>
2025-06-01 17:54:24 -0300

    Finaliza código JavaScript em arquivo atividade-26.js

--- atividade-26.js
+++ atividade-26.js
@@ -1,1 +1,17 @@
+/* 26 – Histórico de navegação
+A equipe de UX quer analisar o último acesso do usuário e os passos anteriores no sistema. Inverta a ordem do histórico para mostrar o mais recente no topo.
+*/
 
+let historico = [
+  "login",
+  "painel",
+  "relatórios",
+  "detalhes do cliente",
+  "editar dados",
+  "configurações",
+  "logout"
+];
+
+let historicoMaisRecentes = historico.reverse();
+
+console.log(`Histórico mais recentes: ${historicoMaisRecentes}`);
