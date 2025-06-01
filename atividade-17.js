4fb3fc67f8d8b553c3502da7bef5047e93c48d56
Lucas <lucastavaresbento@gmail.com>
2025-06-01 16:51:50 -0300

    Finaliza código JavaScript em arquivo atividade-17.js

---
+++ atividade-17.js
@@ -0,0 +1,19 @@
+/*  17 – Atualização de lista de usuários
+Você recebeu dois arrays com usuários de sistemas diferentes que agora estão sendo fundidos. Crie uma nova lista combinando todos os usuários.
+*/
+
+let usuariosSistemaA = [
+  "aline.souza",
+  "maria.ferreira",
+  "carlos.lima",
+  "ana.martins"
+];
+
+let usuariosSistemaB = [
+  "joao.silva",
+  "paulo.pereira",
+  "fernanda.alves",
+  "marcelo.cardoso"
+];
+
+console.log(`Lista combinada: ${usuariosSistemaA.concat(usuariosSistemaB).join(', ')}`);
