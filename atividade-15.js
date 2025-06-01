62bb8783c70068462e94d12e047ecb9a85fc6d85
Lucas <lucastavaresbento@gmail.com>
2025-06-01 16:34:56 -0300

    Finaliza código JavaScript em arquivo atividade-15.js

--- atividade-15.js
+++ atividade-15.js
@@ -1,1 +1,18 @@
+/* 15 – Remoção de itens inativos no menu
+Dois itens do menu de navegação estão obsoletos e precisam ser removidos da posição 2 e 3, pois o sistema não oferece mais esses serviços.
+*/
 
+let menuNavegacao = [
+  "Início",
+  "Serviços",
+  "Portfólio",
+  "Blog",
+  "Contato",
+  "Login",
+  "Área do cliente"
+];
+
+let itensRemovidos = menuNavegacao.splice(2, 2);
+
+console.log(`Depois do splice: ${menuNavegacao}`);
+console.log(`Elementos removidos: ${itensRemovidos}`);
