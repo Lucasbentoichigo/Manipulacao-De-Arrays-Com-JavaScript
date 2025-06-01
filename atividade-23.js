a92205c3b2a9973e6633a0580652bed769559503
Lucas <lucastavaresbento@gmail.com>
2025-06-01 17:43:09 -0300

    Finaliza código JavaScript em arquivo atividade-23.js

---
+++ atividade-23.js
@@ -0,0 +1,19 @@
+/* 23 – Organizar lista de candidatos
+A lista de candidatos para entrevista deve ser organizada em ordem alfabética antes de ser enviada ao RH.
+*/
+
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+
+let listaOrdenada = candidatos.sort();
+
+console.log(`Lista ordenada: ${listaOrdenada}`);
+
