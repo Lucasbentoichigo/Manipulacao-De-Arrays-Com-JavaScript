51d0ef75122de2cca9775ba795ae839b601c6af6
Lucas <lucastavaresbento@gmail.com>
2025-06-01 16:55:24 -0300

    Finaliza código JavaScript em arquivo atividade-18.js

---
+++ atividade-18.js
@@ -0,0 +1,19 @@
+/* 18 – Catálogo unificado
+Um cliente corporativo pediu que fosse feito um catálogo unificado com os produtos de duas categorias: hardware e periféricos. Gere uma nova lista com todos os itens.
+*/
+
+let hardware = [
+  "processador i7",
+  "placa-mãe",
+  "memória RAM 16GB",
+  "SSD 1TB"
+];
+
+let perifericos = [
+  "teclado gamer",
+  "mouse ótico",
+  "webcam HD",
+  "fone com microfone"
+];
+
+console.log(`Catálogo Unificado: ${hardware.concat(perifericos).join(', ')}`);
