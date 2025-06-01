9c054f79e8be9b1c88730e042f8014ffb993da53
Lucas <lucastavaresbento@gmail.com>
2025-06-01 16:43:15 -0300

    Cria arquivo atividade-16.js

---
+++ atividade-16.js
@@ -0,0 +1,17 @@
+/* 16 – Substituição de matérias na grade
+A escola decidiu substituir as disciplinas “Biologia” e “Física” da matriz atual por “Programação” e “Robótica”. Ambas estão na posição 3 e 4 da grade. Faça essa troca.
+*/
+
+let gradeCurricular = [
+  "Matemática",
+  "Português",
+  "Biologia",
+  "Física",
+  "História",
+  "Educação Física",
+  "Inglês"
+];
+
+gradeCurricular[2] = "Programação";
+gradeCurricular[3] = "Robótica";
+console.log(`Nova grade curricular: ${gradeCurricular}`);
